010be807ea6bc875a3822b3aa3775a6e1572ef60
mohamadhosein <mhmk1399@gmail.com>
2024-12-23 18:41:07 +0330

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 // import { sahel } from "@/next-persian-fonts/sahel";
 import { ray } from "@/next-persian-fonts/ray";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   verification: {
-    google: 'googled13c95ed93977bad',
+    google: "googled13c95ed93977bad",
   },
   title: "تومک",
   description:
@@ -21,7 +22,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5FFQTK8C');
+          `}
+        </Script>
+      </head>
       <body className={ray.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5FFQTK8C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Navbar />
 
         {children}
