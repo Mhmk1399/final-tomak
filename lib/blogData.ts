bca11677123dc35623a4769823be259765165143
mohamadhosein <mhmk1399@gmail.com>
2025-01-05 11:14:47 +0330

    s

--- lib/blogData.ts
+++ lib/blogData.ts
@@ -9,16 +9,16 @@ const blogPosts = [
     image: "/assets/images/ai.jpg",
     time: "12:00",
   },
-  // {
-  //   id: 2,
-  //   title: "راهنمای جامع طبقه بندی صدا یا Audio Classification",
-  //   summary:
-  //     "در این مقاله ، راهنمای کاملی برای طبقه بندی صدا یا Audio Classification در زمینه هوش مصنوعی و داده کاوی ارائه می‌شود.",
-  //   date: "1403/10/3",
-  //   link: "/blogs/2",
-  //   image: "/assets/images/overview.jpg",
-  //   time: "10:00",
-  // },
+  {
+    id: 2,
+    title: "تحلیل رگرسیون ها یا Regression Analysis چیست و چه کار برد هایی دارد",
+    summary:
+      "",
+    date: "1403/10/13",
+    link: "/a-beginners-guide-about-what-is-regresion-and-when-does-it-used-in-persian-language",
+    image: "/assets/images/overview.jpg",
+    time: "15:00",
+  },
   // {
   //   id: 3,
   //   title: "راهنمای جامع طبقه بندی تصویر یا Image Classification",
