3ab5e1ad356c5d626ff0f266ee493d12a4498146
mohamadhosein <mhmk1399@gmail.com>
2025-01-10 13:02:30 +0330

    build error

--- components/services/KindOfServices.tsx
+++ components/services/KindOfServices.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 interface ServiceItem {
-  videosrc: string;
+  videosrc?: string;
   id: number;
   title: string;
   icon: string;
